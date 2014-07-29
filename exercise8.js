fa02ab05db28117cb4e3dd2346c4a653879ea900
Larry Price <larry.price.dev@gmail.com>
2014-07-29 19:15:50 -0400

    exercise 8 -http collect

---
+++ exercise8.js
@@ -0,0 +1,17 @@
+var http = require('http');
+
+var result = "";
+
+http.get(process.argv[2], function(response) {
+  response.setEncoding('utf8');
+  response.on("data", function(data) {
+    result += data;
+  });
+  response.on("error", function(error) {
+    result += response.statusCode;
+  });
+  response.on("end", function() {
+    console.log(result.length);
+    console.log(result);
+  });
+});
